33714ed74e0c134b326d5fc5bf5d8b9ba4fc2911
TheodorVimes <petrovfedor@googlemail.com>
2019-07-16 23:07:13 +0400

    Added Config Server
     - application configs moved to yml

--- empire-troops-service/src/main/java/com/empire/controller/TroopsController.java
+++ empire-troops-service/src/main/java/com/empire/controller/TroopsController.java
@@ -2,6 +2,8 @@ package com.empire.controller;
 
 import com.empire.model.Squad;
 import com.empire.service.TroopsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TroopsController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TroopsController.class);
+
 	@Autowired
 	private TroopsService troopsService;
 
 	@GetMapping(value = "/getSquad", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Squad> getSquad() {
-		System.out.println("Going for Troopers...");
+		LOGGER.info("Going for Troopers...");
 		return ResponseEntity.ok(troopsService.createSquad());
 	}
 
