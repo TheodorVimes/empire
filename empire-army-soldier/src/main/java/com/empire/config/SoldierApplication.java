864a66887afeaaa277e8d9db8ef246e9c2369aaf
TheodorVimes <petrovfedor@googlemail.com>
2019-07-16 23:38:35 +0400

    Turning off Discovery Server from flow

--- empire-army-soldier/src/main/java/com/empire/config/SoldierApplication.java
+++ empire-army-soldier/src/main/java/com/empire/config/SoldierApplication.java
@@ -2,10 +2,8 @@ package com.empire.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.empire")
-@EnableDiscoveryClient
 public class SoldierApplication {
 
 	public static void main(String[] args) {
--- empire-troops-service/src/main/java/com/empire/config/TroopsApplication.java
+++ empire-troops-service/src/main/java/com/empire/config/TroopsApplication.java
@@ -3,14 +3,12 @@ package com.empire.config;
 import com.empire.model.Soldier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication(scanBasePackages = {"com.empire"})
-@EnableDiscoveryClient
 @EnableFeignClients
 public class TroopsApplication {
 
@@ -18,7 +16,7 @@ public class TroopsApplication {
 		SpringApplication.run(TroopsApplication.class);
 	}
 
-	@FeignClient(name = "soldier")
+	@FeignClient(url = "localhost:8081", name = "soldier")
 	public interface SoldierServiceClient {
 
 		@RequestMapping(value = "/getSoldier", method = RequestMethod.GET)
